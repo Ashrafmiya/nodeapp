0a23719d0b06104c148706409a7c51872b09a873
Ashrafmiya <ashrafmiya4@gmail.com>
2023-10-09 11:28:46 +0530

    first code

---
+++ index.js
@@ -0,0 +1,16 @@
+//importing node framework
+varexpress=require("express");
+ 
+varapp=express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get("/", function (req, res) {​​​​​​​​
+res.send("hello world");
+}​​​​​​​​);
+//listen to port 80 by default
+app.listen(process.env.PORT||80);
+ 
+module.exports=app;
+
+
+
+
